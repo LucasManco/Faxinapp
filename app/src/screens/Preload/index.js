9844daf5b5f92859f0c9449dd00babb463b250e4
Lucas Souza <lucas.souza@resultate.com.br>
2022-03-24 23:59:14 -0300

    Starting project

---
+++ app/src/screens/Preload/index.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useContext } from 'react';
+import { Container, LoadingIcon } from './styles';
+import AsyncStorage from '@react-native-community/async-storage';
+import { useNavigation } from '@react-navigation/native';
+
+import { UserContext } from '../../contexts/UserContext';
+
+
+import FaxinaLogo from '../../assets/faxina.svg';
+
+export default () => {
+
+    // const { dispatch: userDispatch } = useContext(UserContext);
+    const navigation = useNavigation();
+
+    useEffect(()=>{
+        const checkToken = async () => {
+            navigation.navigate('SignIn');            
+        }
+        checkToken();
+    }, []);
+
+    return (
+        <Container>
+            <FaxinaLogo width="100%" height="160" />
+            <LoadingIcon size="large" color="#FFFFFF" />
+        </Container>
+    );
+}
